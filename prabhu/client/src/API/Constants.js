40e120f0fa32b34b17539cbb941c5a7d271ff187
prabhu <luckyprabhu2000@gmail.com>
2025-12-20 09:31:53 +0530

    code adding

--- prabhu/client/src/API/Constants.js
+++ prabhu/client/src/API/Constants.js
@@ -1,5 +1,5 @@
 // export const BASE_URL = "https://actimizestudio.onrender.com/"; 
-export const BASE_URL = "http://localhost:3000/"; 
+export const BASE_URL = "https://nodecrud-9hnx.onrender.com/"; 
 
 export const STATUS_CODE = {
   INTERNAL_SERVER_ERROR: 500,
